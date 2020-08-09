44a3919ea9902e48ea80c9c3d71d013ceaffd77f
TruongKhang <truongkhang95@gmail.com>
2020-08-09 21:44:33 +0900

    update

--- CasMVSNet/train_distributed.sh
+++ CasMVSNet/train_distributed.sh
@@ -6,5 +6,5 @@ if [ ! -d $LOG_DIR ]; then
     mkdir -p $LOG_DIR
 fi
 
-python -m torch.distributed.launch --nproc_per_node=$1 train.py --logdir $LOG_DIR --dataset=dtu_yao --batch_size=1 --trainpath=$MVS_TRAINING \
-                --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 ${@:3} | tee -a $LOG_DIR/log.txt
+python -m torch.distributed.launch --nproc_per_node=$1 train.py --logdir $LOG_DIR --dataset=dtu_yao --batch_size=2 --trainpath=$MVS_TRAINING \
+                --trainlist lists/dtu/subset_train.txt --testlist lists/dtu/subset_val.txt --numdepth=192 ${@:3} | tee -a $LOG_DIR/log.txt 
